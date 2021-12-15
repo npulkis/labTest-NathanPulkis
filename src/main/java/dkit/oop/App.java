f48b886bbdecf6cf8f69762b4a42cfd9d4371721
Nathan P <81049599+nthnplks21@users.noreply.github.com>
2021-12-15 11:18:25 +0000

    Q1 finished

--- src/main/java/dkit/oop/App.java
+++ src/main/java/dkit/oop/App.java
@@ -17,11 +17,13 @@ public class App
 
         System.out.println("OOP - CA2 - 20% - December 2021 - AirplaneManager");
 
-        //Q2
-        // CargoAirplane cargoPlane1 = new CargoAirplane("Douglas DC-4",20000);
-        // CargoAirplane cargoPlane2 = new CargoAirplane("Boeing C-7000",25000);
+        //Q1
+         CargoAirplane cargoPlane1 = new CargoAirplane("Douglas DC-4",20000);
+         CargoAirplane cargoPlane2 = new CargoAirplane("Boeing C-7000",25000);
 
         // display the two objects
+        System.out.println(cargoPlane1.toString());
+        System.out.println(cargoPlane2.toString());
 
         AirplaneManager mgr = new AirplaneManager();
 
--- src/main/java/dkit/oop/CargoAirplane.java
+++ src/main/java/dkit/oop/CargoAirplane.java
@@ -1,16 +1,35 @@
 package dkit.oop;
 
-public class CargoAirplane {
+public class CargoAirplane extends Airplane {
+
+
+
+
+
+
 
     // fields (ref. Q1)
 
+    private double MAX_LOAD_KILOS;
+    private double currentLoad;
 
     // constructor
+
+    public CargoAirplane(String type, double MAX_LOAD_KILOS) {
+        super(type);
+        this.MAX_LOAD_KILOS = MAX_LOAD_KILOS;
+    }
+
 
 
     // toString()
 
 
-
+    @Override
+    public String toString() {
+        return "CargoAirplane{" +
+                "MAX_LOAD_KILOS=" + MAX_LOAD_KILOS +
+                "} " + super.toString();
+    }
 } // END of CargoAirplane class.
 
